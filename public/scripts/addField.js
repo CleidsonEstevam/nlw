95281e1c88fea26aa9da9782545c832c3290773f
Cleidson <cleidsonestevamdasilva@hotmail.com>
2020-08-08 00:00:26 -0300

    Primeiro commit

---
+++ public/scripts/addField.js
@@ -0,0 +1,21 @@
+//Procurar o botão  
+document.querySelector("#add-time") 
+//quando clicar no botão
+.addEventListener('click', cloneField)
+//executar uma ação
+function cloneField() {
+  //duplicar os campos.. que campo?
+const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+//limpar os campos.. que campo?
+const fields = newFieldContainer.querySelectorAll('input')
+
+//para cada campo, limpar 
+fields.forEach(function(field){
+    //pegar o field do momento e limpa ele
+    field.value = ""
+})
+
+//colocar na pagina.. onde?
+document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
